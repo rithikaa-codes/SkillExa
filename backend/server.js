f83a023ead76f81fab1ca4e055b63040842682bb
Rithikaa K <rithikaa-codes@users.noreply.github.com>
2026-04-07 09:48:09 +0530

    Fix Express 5 wildcard route for production

--- backend/server.js
+++ backend/server.js
@@ -68,7 +68,7 @@ if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
   });
 }
